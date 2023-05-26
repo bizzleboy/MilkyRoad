5802295e6f66dd53938314f1113a5f66dde2f04f
panharathsok3 <panharath.sok123@gmail.com>
2023-05-26 12:07:20 -0400

    added fps stuff

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 10f;
+    public float minDistance = 2;
+    public int damageAmount = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
