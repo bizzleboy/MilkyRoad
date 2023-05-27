b04a408eb2a29030710233924455e0130df1ddc6
Parker Brandt <brandt.p@northeastern.edu>
2023-05-27 12:36:40 -0400

    mmm

--- Assets/Scripts/ScooterController.cs
+++ Assets/Scripts/ScooterController.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ScooterController : MonoBehaviour
 {
-    public float accelerationRate = 1f;    // Rate at which scooter accelerates
-    public float maxSpeed = 10f;           // Maximum speed scooter can attain
-    public float minSpeedForTurn = 2f;     // Minimum speed for scooter to be able to turn
-    public float turnSpeed = 100f;         // Turning speed of scooter
-    public float gravity = 9.8f;           // Gravity value
+    public float accelerationRate = 1f;
+    public float maxSpeed = 10f;
+    public float minSpeedForTurn = 2f;
+    public float turnSpeed = 100f;
+    public float gravity = 9.8f;
 
-    private CharacterController characterController;  // Reference to CharacterController component
-    private float speed = 0f;                         // The current speed of the scooter
-    private Vector3 moveDirection = Vector3.zero;     // Direction the character will move
+    public float rampForceMultiplier = 5f; // Force multiplier for ramp launch
+
+    private CharacterController characterController;
+    private float speed = 0f;
+    private Vector3 moveDirection = Vector3.zero;
 
     private void Start()
     {
@@ -21,42 +23,44 @@ public class ScooterController : MonoBehaviour
     {
         if (!LevelManager.isGameOver)
         {
-            float verticalInput = Input.GetAxis("Vertical");  // Assume Vertical axis for acceleration
-            float horizontalInput = Input.GetAxis("Horizontal"); // Assume Horizontal axis for turning
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
 
-            // Calculate acceleration and deceleration
             if (verticalInput != 0)
             {
                 speed = Mathf.Clamp(speed + verticalInput * accelerationRate * Time.deltaTime, -maxSpeed, maxSpeed);
             }
             else
             {
-                // decelerate smoothly when no input is given
                 speed = Mathf.Lerp(speed, 0, Time.deltaTime * accelerationRate);
             }
 
-            // Apply turning only if speed is greater than the minimum
             if (Mathf.Abs(speed) > minSpeedForTurn)
             {
                 transform.Rotate(0, horizontalInput * turnSpeed * Time.deltaTime, 0);
             }
 
-            // Check if character controller is grounded
             if (characterController.isGrounded)
             {
                 moveDirection = transform.forward * speed;
             }
 
-            // Apply gravity
             moveDirection.y -= gravity * Time.deltaTime;
 
-            // Apply the calculated movement
             characterController.Move(moveDirection * Time.deltaTime);
         }
         else
         {
             speed = 0;
         }
-       
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("RampEnd")) // tag the ramp end with "RampEnd"
+        {
+            moveDirection.y = speed * rampForceMultiplier; // adjust the launch force
+        }
     }
 }
