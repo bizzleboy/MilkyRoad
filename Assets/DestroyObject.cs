282d92b65f70d82131e8bb5fd7ca101aa6463279
Jessica Shang <jesss.yujie@gmail.com>
2023-05-26 21:01:37 -0400

    Target windows complete

--- Assets/DestroyObject.cs
+++ Assets/DestroyObject.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.left);
     }
 
     // Update is called once per frame
--- Assets/Scripts/WindowHit.cs
+++ Assets/Scripts/WindowHit.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class WindowHit : MonoBehaviour
 {
+    public AudioClip successHitSFX;
+    public GameObject nextTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -26,9 +28,20 @@ public class WindowHit : MonoBehaviour
 
     void Delivered()
     {
-        //Instantiate(dementorExpelled, transform.position, transform.rotation);
-
         gameObject.SetActive(false);
-        Destroy(gameObject, 0.5f);
+
+        if(nextTarget != null) 
+        {
+            Debug.Log("Activated");
+            nextTarget.SetActive(true);
+        }
+
+        AudioSource.PlayClipAtPoint(successHitSFX, Camera.main.transform.position);
+        Destroy(gameObject);
     }
+
+    // public void StartActiveTarget()
+    // {
+    //     gameObject.SetActive(true);
+    // }
 }
