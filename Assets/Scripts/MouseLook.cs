5edf3cdb7815b3fe9dabdc8c99753303a1073837
panharathsok3 <panharath.sok123@gmail.com>
2023-05-27 11:27:03 -0400

    put webgl stuff in

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     Transform playerBody;
-    public float mouseSensitivity = 100;
+    public float mouseSensitivity = 50;
 
     float pitch = 0;
 
