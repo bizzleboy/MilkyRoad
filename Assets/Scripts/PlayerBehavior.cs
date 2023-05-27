80beca5e76c28a30db1a66728548cbcd1a2d2877
panharathsok3 <panharath.sok123@gmail.com>
2023-05-26 23:11:30 -0400

    added level lost when fall off

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public Transform ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.FindGameObjectWithTag("Ground").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!LevelManager.isGameOver)
+        {
+            if (transform.position.y < ground.position.y)
+            {
+                FindObjectOfType<LevelManager>().LevelLost();
+            }
+        }
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         // Replace "Obstacle" with the tag of your obstacle objects
