1c364296ca084f380567c200bd1b78d5d39737d9
panharathsok3 <panharath.sok123@gmail.com>
2023-06-12 16:43:02 -0400

    added the scene swap to shooting

--- Assets/Scripts/NextLevelTrigger.cs
+++ Assets/Scripts/NextLevelTrigger.cs
@@ -7,15 +7,25 @@ public class NextLevelTrigger : MonoBehaviour
     public GameObject sceneChange;
     public GameObject crosshair;
 
+    public bool hasBigBuilding = false;
+    public GameObject bigBuilding;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (hasBigBuilding)
+            {
+                bigBuilding.SetActive(false);
+            }
+
             other.GetComponent<PlayerBehavior>().ChangeToFPS();
 
             crosshair.SetActive(true);
             sceneChange.SetActive(true);
             GameObject.FindGameObjectWithTag("SceneChange").GetComponent<DeliveryManagement>().ActivateLight();
+            
+            gameObject.SetActive(false);
         }
     }
 }
