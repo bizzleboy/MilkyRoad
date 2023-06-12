d79d4593ff7418b965ff10f6aca4fd16d1db8f4a
Parker Brandt <brandt.p@northeastern.edu>
2023-06-12 12:48:13 -0400

    LevelDesign

--- Assets/Scripts/QuickTimeEvent.cs
+++ Assets/Scripts/QuickTimeEvent.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class QuickTimeEvent : MonoBehaviour
 {
-    public Animator enemyAnimator; // enemy animator
-    public string enemyAnimationName; // animation name
-    public int requiredPresses = 15; // required number of key presses
-    public float animationDuration; // duration of animation
+    public Animator animator;
+    public PlayerBehavior player;
+    public float duration = 5f; // change this to your animation's duration
+    private int pressCount;
+    private bool isQteActive;
 
-    private int currentPresses = 0; // counter for current key presses
-    private bool isPlayerDead = false; // flag to indicate if player is dead
-    PlayerBehavior getDeath;
-
-    // use this for initialization
     void Start()
     {
-        // start the QTE
-        StartCoroutine(StartQTE());
-        PlayerBehavior getDeath= GetComponent<PlayerBehavior>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // check for spacebar press and increment counter
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isQteActive)
         {
-            currentPresses++;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                pressCount++;
+                if (pressCount >= 15)
+                {
+                    isQteActive = false;
+                    // Player survives QTE
+                }
+            }
         }
     }
 
-    IEnumerator StartQTE()
+    public void StartQTE()
     {
-        // play the enemy animation
-        enemyAnimator.Play(enemyAnimationName);
-
-        // wait for the duration of the animation
-        yield return new WaitForSeconds(animationDuration);
-
-        // if the player didn't press space enough times, they die
-        if (currentPresses < requiredPresses)
-        {
-            getDeath.PlayerDies();
-        }
+        isQteActive = true;
+        animator.Play("Crawl"); // replace with your animation's name
+        StartCoroutine(QteCountdown());
     }
 
- 
+    IEnumerator QteCountdown()
+    {
+        yield return new WaitForSeconds(duration);
+        if (isQteActive)
+        {
+            // Player didn't press space enough times, so they die
+            player.PlayerDies();
+        }
+    }
 }
