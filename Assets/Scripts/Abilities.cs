c34750cbc7ae526f18fa794095bf2fa257659a0d
panharathsok3 <panharath.sok123@gmail.com>
2023-06-20 19:50:22 -0400

    added a UI for the light and how much power it has

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Abilities : MonoBehaviour
 {
@@ -8,11 +9,16 @@ public class Abilities : MonoBehaviour
     public AudioClip LigthSwitchSFX;
 
     public GameObject lights;
+    public Slider lightSlider;
+
+    public float decreasePower = 0.2f;
+    public float increasePower = 0.1f;
 
     bool lightOn;
     bool lightCharging;
 
     float timer;
+    float lightPower;
     float stopTime = 2f;
 
     private void Start()
@@ -20,11 +26,14 @@ public class Abilities : MonoBehaviour
         lightOn = false;
         lightCharging = false;
         timer = 0;
+        lightPower = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        lightSlider.value = lightPower;
+
         if (!PlayerBehavior.fpsMode)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -38,8 +47,50 @@ public class Abilities : MonoBehaviour
             }
         }
 
-        
 
+        HandleLightSlider();
+
+        HandleTimer();
+
+    }
+
+    void SwitchLight()
+    {
+        if (lightOn)
+        {
+            lights.SetActive(false);
+            lightOn = !lightOn;
+        }
+        else
+        {
+            lights.SetActive(true);
+            lightOn = !lightOn;
+        }
+    }
+
+    void HandleLightSlider()
+    {
+        if (lightOn)
+        {
+            lightPower -= decreasePower;
+            if (lightPower <= 0)
+            {
+                lightOn = true;
+                SwitchLight();
+            }
+        }
+        else
+        {
+            lightPower += increasePower;
+            if (lightPower >= 100)
+            {
+                lightPower = 100;
+            }
+        }
+    }
+
+    void HandleTimer()
+    {
         if (LightBehavior.lightHitRatFromSource)
         {
             timer = 0;
@@ -56,21 +107,6 @@ public class Abilities : MonoBehaviour
         {
             RatChaser.lightHit = false;
             timer = 0;
-        }
-
-    }
-
-    void SwitchLight()
-    {
-        if (lightOn)
-        {
-            lights.SetActive(false);
-            lightOn = !lightOn;
-        }
-        else
-        {
-            lights.SetActive(true);
-            lightOn = !lightOn;
         }
     }
 }
